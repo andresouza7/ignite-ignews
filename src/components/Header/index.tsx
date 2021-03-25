087b82673819af9b803b5cdac9eb7858bd67dbf9
Andre Souza <souza.andre1993@gmail.com>
2021-03-25 20:50:11 -0300

    remove prefetch attribute

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,7 +15,7 @@ export function Header() {
           <ActiveLink activeLinkClassName={styles.active} href="/">
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink activeLinkClassName={styles.active} href="/posts" prefetch>
+          <ActiveLink activeLinkClassName={styles.active} href="/posts">
             <a>Posts</a>
           </ActiveLink>
         </nav>
